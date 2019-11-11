6ad6597e252ec041946679049cf2ed574381eef9
beygee <doug0476@naver.com>
2019-11-11 21:24:05 +0900

    add usePromise

--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
 import styled from "styled-components"
 import NewsItem from "./NewsItem"
 import axios from "axios"
+import usePromsie from "../lib/usePromise"
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -19,35 +20,26 @@ const NewsListBlock = styled.div`
 const API_KEY = "2e2682361adc4777a97f72b81f6bbb00"
 
 const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null)
-  const [loading, setLoading] = useState(false)
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true)
-      try {
-        const query = category === "all" ? "" : `&category=${category}`
-        const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=${API_KEY}`
-        )
-
-        setArticles(response.data.articles)
-      } catch (e) {
-        console.error(e)
-      }
-      setLoading(false)
-    }
-
-    fetchData()
+  const [loading, response, error] = usePromsie(() => {
+    const query = category === "all" ? "" : `&category=${category}`
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=${API_KEY}`
+    )
   }, [category])
 
   if (loading) {
     return <NewsListBlock>대기 중 ...</NewsListBlock>
   }
 
-  if (!articles) {
+  if (!response) {
     return null
   }
+
+  if (error) {
+    return <NewsListBlock>에러 발생!</NewsListBlock>
+  }
+
+  const { articles } = response.data
 
   return (
     <NewsListBlock>
