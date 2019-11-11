e5d0a3b846f2742a3cb366b93b82f245c099961c
beygee <doug0476@naver.com>
2019-11-11 21:11:04 +0900

    Category 분기

--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -18,7 +18,7 @@ const NewsListBlock = styled.div`
 
 const API_KEY = "2e2682361adc4777a97f72b81f6bbb00"
 
-const NewsList = () => {
+const NewsList = ({ category }) => {
   const [articles, setArticles] = useState(null)
   const [loading, setLoading] = useState(false)
 
@@ -26,8 +26,9 @@ const NewsList = () => {
     const fetchData = async () => {
       setLoading(true)
       try {
+        const query = category === "all" ? "" : `&category=${category}`
         const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
+          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=${API_KEY}`
         )
 
         setArticles(response.data.articles)
@@ -38,7 +39,7 @@ const NewsList = () => {
     }
 
     fetchData()
-  }, [])
+  }, [category])
 
   if (loading) {
     return <NewsListBlock>대기 중 ...</NewsListBlock>
